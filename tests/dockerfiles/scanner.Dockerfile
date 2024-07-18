1c399958b46dfdd25e364f8e15ad72f6e62e928c
Aviral Takkar <39969667+avtakkar@users.noreply.github.com>
2024-07-18 16:07:35 -0700

    fix, test: fix SAS parser, scrape prom metrics from scanner (#54)
    * test: scrape prom metrics from scanner
    
    * fix: handle error properly

--- tests/dockerfiles/scanner.Dockerfile
+++ tests/dockerfiles/scanner.Dockerfile
@@ -1,10 +1,10 @@
-FROM mcr.microsoft.com/cbl-mariner/base/core:2.0 as scannerbase
+FROM mcr.microsoft.com/cbl-mariner/base/core:2.0 AS scannerbase
 
 ARG FILE_PATH=/usr/local/bin/scannerbase
 
 RUN dd if=/dev/urandom of=$FILE_PATH bs=1 count=$((600 * 1024 * 1024))
 
-FROM mcr.microsoft.com/oss/go/microsoft/golang:1.21-fips-cbl-mariner2.0 as builder
+FROM mcr.microsoft.com/oss/go/microsoft/golang:1.22-fips-cbl-mariner2.0 as builder
 
 COPY ./ /src/
 
@@ -28,5 +28,7 @@ RUN groupadd -g $USER_ID scanner && \
 
 COPY --from=scannerbase --chown=scanner:root /usr/local/bin/scannerbase /usr/local/bin/scannerbase
 COPY --from=builder --chown=scanner:root /src/bin/tests/tests /src/bin/tests/tests
+
+EXPOSE 5004
 
 ENTRYPOINT ["/src/bin/tests/tests", "scanner"]
--- tests/scanner/scanner.go
+++ tests/scanner/scanner.go
@@ -6,9 +6,16 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/binary"
+	"errors"
+	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"time"
+
+	pcontext "github.com/azure/peerd/pkg/context"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/rs/zerolog"
 	"github.com/schollz/progressbar/v3"
@@ -20,6 +27,22 @@ const (
 
 func Scanner(ctx context.Context) error {
 	l := zerolog.Ctx(ctx)
+
+	byteThroughputHist := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "scanner_bytes_throughput_mib_per_second",
+		Help:    "Speed of scan in Mib per second.",
+		Buckets: prometheus.ExponentialBuckets(1, 1.1, 100),
+	}, []string{"self", "op"})
+	prometheus.DefaultRegisterer.MustRegister(byteThroughputHist)
+
+	errChan := make(chan error, 1)
+
+	go func() {
+		http.Handle("/metrics/prometheus", promhttp.Handler())
+		if err := http.ListenAndServe("0.0.0.0:5004", nil); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			errChan <- err
+		}
+	}()
 
 	sleep(l)
 
@@ -37,7 +60,7 @@ func Scanner(ctx context.Context) error {
 	size := info.Size()
 
 	bar := progressbar.DefaultBytes(size, "reading")
-
+	measure(bar, time.Now(), l, byteThroughputHist, float64(size))
 	w, err := io.Copy(io.MultiWriter(io.Discard, bar), f)
 	if err != nil {
 		l.Fatal().Err(err).Msg("failed to read file")
@@ -46,7 +69,32 @@ func Scanner(ctx context.Context) error {
 		l.Info().Int64("size", size).Int64("read", w).Msg("complete")
 	}
 
-	return nil
+	if err := <-errChan; err != nil {
+		l.Error().Msg(fmt.Sprintf("prom error: %v", err))
+	}
+
+	l.Info().Msg("sleeping to allow metrics scraping")
+	time.Sleep(24 * 365 * time.Hour)
+
+	return err
+}
+
+func measure(bar *progressbar.ProgressBar, startTime time.Time, l *zerolog.Logger, byteThroughputHist *prometheus.HistogramVec, size float64) {
+	go func() {
+		for {
+			time.Sleep(500 * time.Microsecond)
+			count := bar.State().CurrentBytes
+
+			if count >= size {
+				break
+			}
+
+			elapsed := time.Since(startTime)
+			speed := float64(count) / elapsed.Seconds()
+			l.Debug().Float64("speed", speed).Float64("bytes", count).Dur("elapsed", elapsed).Msg("speed")
+			byteThroughputHist.WithLabelValues(pcontext.NodeName, "read").Observe(speed / float64(1024*1024))
+		}
+	}()
 }
 
 func sleep(l *zerolog.Logger) {
